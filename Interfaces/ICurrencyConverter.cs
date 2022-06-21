ed1af7f59855e0403469b184b44a0abca80e1af2
kami-jami <kami.jami@gmail.com>
2022-06-22 00:43:56 +0430

    first commit

---
+++ Interfaces/ICurrencyConverter.cs
@@ -0,0 +1,20 @@
+namespace CurrencyConverter.Interfaces
+{
+    public interface ICurrencyConverter
+    {
+        /// <summary>
+        /// Clears any prior configuration. 
+        /// </summary> void ClearConfiguration();
+        void ClearConfiguration();
+
+        /// <summary> 
+        /// Updates the configuration. Rates are inserted or replaced internally. 
+        /// </summary> void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates); 
+        void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates);
+
+        /// <summary> 
+        /// Converts the specified amount to the desired currency. 
+        /// </summary> double Convert(string fromCurrency, string toCurrency, double amount);
+        double Convert(string fromCurrency, string toCurrency, double amount);
+    }
+}
